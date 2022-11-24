bbbcdfaf600e31f973375f508efbbe3f7a5911da
Roger Puig <rogerpb1997@gmail.com>
2022-11-24 13:48:11 +0100

    ejercicio 11

---
+++ SB11/src/test/java/com/sb11/Sb11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
